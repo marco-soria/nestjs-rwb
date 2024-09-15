aefaa1960185f2f727fa690d5c3b2dc79b44a233
marco-soria <marco.soriaa7@gmail.com>
2024-09-14 22:42:29 -0500

    changed script and datasource for compilation

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,6 +8,6 @@ dotenvExpand.expand(dotenv.config());
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATASOURCE_URL,
-  entities: [join(__dirname, '/../domain/**/entities/*.entity{.ts,.js}')],
-  migrations: [join(__dirname, '/../database/migrations/*{.ts,.js}')],
+  entities: [join(__dirname, '/../domain/**/entities/*.entity.js')],
+  migrations: [join(__dirname, '/../database/migrations/*.js')],
 });
--- src/database/migrations/1726371401535-create-tags.ts
+++ src/database/migrations/1726371401535-create-tags.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTags1726370082447 implements MigrationInterface {
-    name = 'CreateTags1726370082447'
+export class CreateTags1726371401535 implements MigrationInterface {
+    name = 'CreateTags1726371401535'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "tag_entity" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_98efc66e2a1ce7fa1425e21e468" PRIMARY KEY ("id"))`);
