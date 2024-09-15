55bae3da647021b321b1ed52209e27aacff5b4b5
marco-soria <marco.soriaa7@gmail.com>
2024-09-14 20:54:38 -0500

    installed docker and made configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
-import { TagModule } from '@app/tag/tag..module';
+
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagsModule } from '@app/tags/tags.module';
+import { EnvModule } from './env/env.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TagModule],
+  imports: [TagsModule, EnvModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
