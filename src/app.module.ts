85ff9898ae94bb5822adc72d2fbaf208f0d6497d
marco-soria <marco.soriaa7@gmail.com>
2024-09-15 10:19:35 -0500

    added users mod

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnvModule } from '@app/env/env.module';
 import { DatabaseModule } from '@app/database/database.module';
 import { TagsModule } from '@app/domain/tags/tags.module';
+import { UsersModule } from '@app/domain/users/users.module';
 
 @Module({
-  imports: [TagsModule, EnvModule, DatabaseModule],
+  imports: [TagsModule, EnvModule, DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
