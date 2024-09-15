27c58cfe5dbf6b17dcfc2257d96add79a1dd68d2
marco-soria <marco.soriaa7@gmail.com>
2024-09-15 09:08:04 -0500

    updated mgiration script

--- src/database/migrations/1726409253280-create-tags.ts
+++ src/database/migrations/1726409253280-create-tags.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTags1726371401535 implements MigrationInterface {
-    name = 'CreateTags1726371401535'
+export class CreateTags1726409253280 implements MigrationInterface {
+    name = 'CreateTags1726409253280'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "tag_entity" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_98efc66e2a1ce7fa1425e21e468" PRIMARY KEY ("id"))`);
